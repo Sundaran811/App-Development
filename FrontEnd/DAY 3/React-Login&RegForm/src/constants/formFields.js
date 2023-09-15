bf203efce3ef2a4192c2514c0322c1daf073fb3d
Sundaran811 <nsundaran811@gmail.com>
2023-09-15 08:29:24 +0530

    UML diagrams and Login&Regform are done.

---
+++ FrontEnd/DAY 3/React-Login&RegForm/src/constants/formFields.js
@@ -0,0 +1,77 @@
+const loginFields=[
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    }
+]
+
+const signupFields=[
+    {
+        labelText:"Username",
+        labelFor:"username",
+        id:"username",
+        name:"username",
+        type:"text",
+        autoComplete:"username",
+        isRequired:true,
+        placeholder:"Full name"   
+    },
+    {
+        labelText:"Phone Number",
+        labelFor:"phoneno",
+        id:"phoneno",
+        name:"phoneno",
+        type:Number,
+        autoComplete:Number,
+        isRequired:true,
+        placeholder:"Contact Number"   
+    },
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    },
+    {
+        labelText:"Confirm Password",
+        labelFor:"confirm-password",
+        id:"confirm-password",
+        name:"confirm-password",
+        type:"password",
+        autoComplete:"confirm-password",
+        isRequired:true,
+        placeholder:"Confirm Password"   
+    }
+]
+
+export {loginFields,signupFields}
